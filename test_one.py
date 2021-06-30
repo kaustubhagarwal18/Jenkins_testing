8b5eac08214fbfb614cbffb6dd2c3a99347273a2
Kaustubh Agarwal <kaustubhagarwal18@gmail.com>
2021-06-30 11:16:33 +0200

    Update test_one.py

--- test_one.py
+++ test_one.py
@@ -5,7 +5,7 @@ class SimpleTest(unittest.TestCase):
     # Returns True or False. 
     def test(self):        
         a = 1
-        self.assertEqual(a,1)
+        self.assertEqual(a,2)
   
 if __name__ == '__main__':
     unittest.main()
